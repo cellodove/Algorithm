fe454f39806361bf70417bd9d90b739b51dc8b33
wntjd200@naver.com <wntjd200@naver.com>
2022-02-05 22:46:04 +0900

    init

---
+++ src/string/change_upper_lower/Main.java
@@ -0,0 +1,25 @@
+package string.change_upper_lower;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.print(T.solution(str));
+    }
+
+    public String solution(String str){
+        String answer = "";
+
+        for(char x : str.toCharArray()){
+            if (Character.isLowerCase(x)){
+                answer += Character.toUpperCase(x);
+            }else {
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+}
