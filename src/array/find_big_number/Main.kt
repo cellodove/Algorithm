f268036af27552087e3de746de908c2f71199004
wntjd200@naver.com <wntjd200@naver.com>
2022-02-24 00:09:30 +0900

    find_big_number

---
+++ src/array/find_big_number/Main.kt
@@ -0,0 +1,35 @@
+package array.find_big_number
+
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.jvm.JvmStatic
+
+class Main {
+    fun solution(n : Int,arr: ArrayList<Int>): ArrayList<Int> {
+        var answer = arrayListOf<Int>()
+
+        answer.add(arr[0])
+        for (i in 1 until  n){
+            if (arr[i]>arr[i-1]){
+                answer.add(arr[i])
+            }
+        }
+        return answer
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val T = Main()
+            val kb = Scanner(System.`in`)
+            val n = kb.nextInt()
+            val arr = arrayListOf<Int>()
+            for (i in 0 until n){
+                arr.add(kb.nextInt())
+            }
+            for (x in T.solution(n,arr)){
+                print("$x ")
+            }
+        }
+    }
+}
