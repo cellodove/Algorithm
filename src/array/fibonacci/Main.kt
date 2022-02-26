6587018b999bb4894dc159fc00242e5dd358709f
wntjd200@naver.com <wntjd200@naver.com>
2022-02-26 18:45:32 +0900

    fibonacci

---
+++ src/array/fibonacci/Main.kt
@@ -0,0 +1,30 @@
+package array.fibonacci
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Main {
+    fun solution(n : Int): ArrayList<Int> {
+        var answer = arrayListOf<Int>()
+        answer.add(1)
+        answer.add(1)
+
+        for (i in 2 until n){
+            answer.add(answer[i-2] + answer[i-1])
+        }
+        return answer
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val T = Main()
+            val kb = Scanner(System.`in`)
+            val n = kb.nextInt()
+
+            for (x in T.solution(n)){
+                print("$x ")
+            }
+        }
+    }
+}
