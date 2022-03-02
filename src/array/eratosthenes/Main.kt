a6d8c9d1561b919697cef0423966bd82461bd955
wntjd200@naver.com <wntjd200@naver.com>
2022-03-02 23:42:01 +0900

    eratosthenes

---
+++ src/array/eratosthenes/Main.kt
@@ -0,0 +1,31 @@
+package array.eratosthenes
+
+import java.util.*
+
+internal class Main {
+    fun solution(n: Int): Int {
+        var cnt = 0
+        val ch = IntArray(n + 1)
+        for (i in 2..n) {
+            if (ch[i] == 0) {
+                cnt++
+                var j = i
+                while (j <= n) {
+                    ch[j] = 1
+                    j += i
+                }
+            }
+        }
+        return cnt
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val T = Main()
+            val kb = Scanner(System.`in`)
+            val n = kb.nextInt()
+            println(T.solution(n))
+        }
+    }
+}
