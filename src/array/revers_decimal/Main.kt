81554fd6e9f403c5a46481f5f9e70f2d1be61a1f
wntjd200@naver.com <wntjd200@naver.com>
2022-03-14 22:14:39 +0900

    revers_decimal

---
+++ src/array/revers_decimal/Main.kt
@@ -0,0 +1,51 @@
+package array.revers_decimal
+
+import java.util.*
+
+
+internal class Main {
+    fun isPrime(num : Int): Boolean{
+        if (num==1){
+            return false
+        }
+        for (i in 2 until num){
+            if (num%i==0){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun solution(n: Int,arr :ArrayList<Int>): ArrayList<Int> {
+        var answer = arrayListOf<Int>()
+        for (i in 0 until n){
+            var tmp = arr[i]
+            var res = 0
+            while (tmp>0){
+                var t = tmp%10
+                res = res*10+t
+                tmp /= 10
+            }
+            if (isPrime(res)){
+                answer.add(res)
+            }
+        }
+        return answer
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val T = Main()
+            val kb = Scanner(System.`in`)
+            val n = kb.nextInt()
+            var arr = arrayListOf<Int>()
+            for (i in 0 until n){
+                arr.add(kb.nextInt())
+            }
+            for (x in T.solution(n,arr)){
+                print("$x ")
+            }
+        }
+    }
+}
