c659980f98fb69494bd3f27f0d76d008f2412855
jusung.kim <wntjd200@naver.com>
2022-12-09 10:11:37 +0900

    .

---
+++ src/fast_campuse/a05_hash_table/MyHash2.java
@@ -0,0 +1,69 @@
+package fast_campuse.a05_hash_table;
+
+public class MyHash2 {
+
+    public Slot[] hashTable;
+
+    public MyHash2(Integer size){
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot{
+        String key;
+        Slot next;
+        String value;
+        Slot(String key, String value){
+            this.key = key;
+            this.next = null;
+            this.value = value;
+        }
+    }
+
+    public int hashFunc(String key){
+        return (int)key.charAt(0) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value){
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null){
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null){
+                if (findSlot.key == key){
+                    findSlot.value = value;
+                    return true;
+                }else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value);
+        }else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key){
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address]!= null){
+            Slot findSlot = this.hashTable[address];
+            while (findSlot != null){
+                if (findSlot.key == key){
+                    return findSlot.value;
+                }else {
+                    findSlot = findSlot.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        MyHash2 myHash = new MyHash2(20);
+        myHash.saveData("kim","01011112222");
+        myHash.saveData("kimm","01055556666");
+        myHash.saveData("lee","01033334444");
+        System.out.println(myHash.getData("kimm"));
+    }
+}
