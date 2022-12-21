05ebd80e9bfddd38da24ec0b676a000e23ef219c
jusung.kim <wntjd200@naver.com>
2022-12-21 16:38:29 +0900

    .

--- src/fast_campuse/bubble_sort/BubbleSort.java
+++ src/fast_campuse/bubble_sort/BubbleSort.java
@@ -6,14 +6,19 @@ import java.util.Collections;
 public class BubbleSort {
 
     public ArrayList<Integer> sort(ArrayList<Integer> arrayList){
-        while (true){
+
+        //한번 반복할 때마 가장큰수는 배열의 가장 마지막 부분으로 밀린다.
+        for (int index = 0; index < arrayList.size()-1; index++){
             boolean swap = false;
-            for (int i = 0; i<arrayList.size()-1; i++){
+            //한번의 반복마다 가장 뒤의 인덱스는 비교하지 않아도 된다.
+            for (int i = 0; i<arrayList.size() - 1 - index; i++){
+                //만약 뒤의 인덱스가 더 크면 서로 위치를 바꾼다.
                 if (arrayList.get(i)>arrayList.get(i+1)){
                     Collections.swap(arrayList, i,i+1);
                     swap = true;
                 }
             }
+            //만약 한번도 위치 교환이 이루어지지 않으면 정렬이 되었다 판단하고 반목문을 나온다.
             if (!swap){
                 break;
             }
@@ -23,16 +28,10 @@ public class BubbleSort {
 
 
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(5);
-        arrayList.add(9);
-        arrayList.add(3);
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(8);
-        arrayList.add(4);
-        arrayList.add(7);
-        arrayList.add(6);
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 0; i < 100; i++) {
+            arrayList.add((int)(Math.random() * 100));
+        }
 
         BubbleSort bubbleSort = new BubbleSort();
         System.out.println(bubbleSort.sort(arrayList));
