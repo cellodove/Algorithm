e7e26e86d53a4b5ada74a6b4f4859f717f66fa04
jusung.kim <wntjd200@naver.com>
2023-01-18 17:56:19 +0900

    .

--- src/fast_campuse/a11_recursive_call/Factorial2.java
+++ src/fast_campuse/a11_recursive_call/Factorial2.java
@@ -1,26 +1,17 @@
 package fast_campuse.a11_recursive_call;
 
-import java.util.ArrayList;
-
 public class Factorial2 {
 
-    public Integer unFactorial(ArrayList<Integer> dataList){
-        if (dataList.size() == 0) {
-            return 0;
+    public Integer unFactorial(Integer number){
+        if (number<=1){
+            return 1;
+        }else {
+            return number * this.unFactorial(number - 1);
         }
-        return dataList.get(0) + this.unFactorial(new ArrayList<Integer>(dataList.subList(1, dataList.size())));
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> testData = new ArrayList();
-        for (int data = 0; data < 10; data++) {
-            testData.add(data);
-        }
-        System.out.println(testData);
-        System.out.println(testData.subList(1, testData.size() - 1));
-
-
         Factorial2 factorial2 = new Factorial2();
-        System.out.println(factorial2.unFactorial(testData));
+        System.out.println(factorial2.unFactorial(31));
     }
 }
