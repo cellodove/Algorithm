5a088f4935d7f719cdbb11175ac026378f961ff9
wntjd200@naver.com <wntjd200@naver.com>
2023-04-30 13:13:12 +0900

    .

---
+++ src/fast_campuse/a14_squential_search/SequentialSearch.java
@@ -0,0 +1,26 @@
+package fast_campuse.a14_squential_search;
+
+import java.util.ArrayList;
+
+public class SequentialSearch {
+
+    public Integer SearchNumberFun(ArrayList<Integer> integers, int searchNumber){
+
+        for (int index = 0; index < integers.size(); index++){
+            if (integers.get(index) == searchNumber){
+                return integers.get(index);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> testData = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            testData.add((int)(Math.random() * 100));
+        }
+        SequentialSearch sequentialSearch = new SequentialSearch();
+        System.out.println(sequentialSearch.SearchNumberFun(testData, 63));
+    }
+
+}
